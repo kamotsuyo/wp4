2254073ff4b29b0fe0746b30ec767d90b4c1c0a9
kamo(mac) <kamotsuyo@gmail.com>
2018-05-09 14:54:22 +0900

    require.jsを使ったXHRのテスト
    header-insert.phpでスクリプトファイルを読み込み
    main.jsからXHRインスタンス化＝＞post.phpへ通信
    console.logに結果表示。

--- httpdocs/wp-content/themes/easiest-wp/header-insert.php
+++ httpdocs/wp-content/themes/easiest-wp/header-insert.php
@@ -1,3 +1,2 @@
-<!-- テーマフォルダに/js/main.jsを配置している場合の読み込み -->
-<?PHP $script_file = get_theme_file_uri().'/js/main.js'; ?>
-<script src='<?PHP echo $script_file; ?>'></script>
+<?PHP
+echo '<script data-main = "' . get_theme_file_uri() . '/js/main.js" src = "' . get_theme_file_uri() . '/js/require.js"></script>';
--- httpdocs/wp-content/themes/easiest-wp/js/main.js
+++ httpdocs/wp-content/themes/easiest-wp/js/main.js
@@ -1,14 +1,21 @@
-"use strict";
+/* global document,location,require */
 (function () {
-    if (document.body) {
-        init();
-    } else {
-        document.addEventListener('DOMContentLoaded', init, false);
+    "use strict";
 
-    }
+    var url_path = (
+        document.currentScript ?
+        document.currentScript.src :
+        document.getElementsByTagName('script')[document.getElementsByTagName('script').length - 1].src
+    ).replace(new RegExp('^' + location.origin), '').replace(/[^\/]+$/, '');
 
-    function init() {
-        console.log('start');
-    }
+    //    console.log(url_path);
+
+    var callback = function (res) {
+        console.log(res);
+    };
+    require([url_path + 'XHR.js'], function (XHR) {
+        var xhr = new XHR(url_path + "../post.php", callback);
+        xhr.get("hoge=hogehoge");
+    });
 
 }());
